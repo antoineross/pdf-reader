6b790ac5cc3ad02b092e7e8a2690d3fc169e5cff
Antoine Ross <amoropesa@up.edu.ph>
2023-07-19 09:59:32 +0800

    edited readme.md file. included an env.example file

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# PDF-Pal
-PDF Pal helps you talk to your PDF/Documents as a chatbot partner. 
+# PDF-Reader
+PDF Reader helps you talk to your PDF/Documents as a chatbot partner. This chatbot partner is powered using an LLM model such as ChatGPT API (paid) and HuggingFace API (free). 
+
+# Installation
+```bash
+cd pdf-reader
+pip install -r requirements.txt
+```
+
+# Usage
+First create a .env file. You can use the .env.example file as a template. Then run the following command on your terminal:
+
+```bash
+streamlit run app.py
+```
--- app.py
+++ app.py
@@ -31,15 +31,15 @@ def get_text_chunks(text):
 
 
 def get_vectorstore(text_chunks):
-    embeddings = OpenAIEmbeddings()
-    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    # embeddings = OpenAIEmbeddings()
+    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
 
 def get_conversation_chain(vectorstore):
-    llm = ChatOpenAI()
-    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+    # llm = ChatOpenAI()
+    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
 
     memory = ConversationBufferMemory(
         memory_key='chat_history', return_messages=True)
